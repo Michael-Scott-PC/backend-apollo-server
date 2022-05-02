b65c9cd6fe7468fa80c87bac0faa63c005f5a713
Michael-Scott-PC <43099976+Michael-Scott-PC@users.noreply.github.com>
2022-05-02 16:51:32 -0400

    Add models to ApolloServer context

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const resolvers = require("./resolvers");
 const typeDefs = require("./schema");
-const db = require("./models");
+const models = require("./models");
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: { models }
 });
 
 server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
   try {
-    db.sequelize.authenticate();
+    models.sequelize.authenticate();
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
